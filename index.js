e28b44f384b8c9a0ffd9dd01ba597e2f32503d21
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-08 12:14:33 -0500

    Calculator Challenge Lab

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function add(a,b){
-    return(a += b)
+    return a += b
 }
 
 function subtract(a,b){
-    return(a -= b)
+    return a -= b
 }
 
 function multiply(a,b){
-    return(a *= b)
+    return a *= b
 }
 
 function divide(a,b){
-    return(a /= b)
+    return a /= b
 }
 
 function increment(a){
-    return(a + 1)
+    return a + 1
 }
 
 function decrement(a){
-    return(a - 1)
+    return a - 1
 }
 function makeInt(n) {
   const x =  parseInt(n, 10)
-    return (x)
+    return x
 }
 function preserveDecimal(n){
     const x = parseFloat(n)
-    return(x)
+    return x
 }
 
